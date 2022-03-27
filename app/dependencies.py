015cb4a0fd9bcf31965954abfc0b9741b387f8a5
bryan-souza <bryans.souza@gmail.com>
2022-03-27 14:21:57 -0300

    Refactored path solving dependency and created a new exception class

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,37 +1,63 @@
 # Built-ins
 import json
 from pathlib import Path
+from typing import Any
 
-# 3rd party
-from fastapi.exceptions import HTTPException
+# Internals
+from app.exceptions import AcessViolationError
 
-# Init
-app_root_path = Path(__file__).resolve()
-config_path   = Path(app_root_path, 'config')
-default_cache_path = Path(app_root_path, 'cache')
 
-default_paths = {
-    "APP_ROOT_PATH": app_root_path,
-    "CONFIG_PATH"  : config_path,
-    "CACHE_PATH"   : default_cache_path
-} 
+class PathRouterMeta(type):
+    _instances = {}
 
-pathspec = { **default_paths }
+    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
-config_file = Path( config_path, 'config.json' )
-if ( config_file.is_file() ):
-    config = json.load( open(config_file) )['paths']
+class PathRouter(object, metaclass=PathRouterMeta):
+    _app_root_path = Path(__file__).resolve()
+    _config_path   = Path(_app_root_path, 'config')
+    _default_cache_path = Path(_app_root_path, 'cache')
+
+    _default_paths = {
+        "APP_ROOT_PATH": _app_root_path,
+        "CONFIG_PATH"  : _config_path,
+        "CACHE_PATH"   : _default_cache_path
+    } 
+
+
+    def __init__(self) -> None:
+        self._pathspec = { **self._default_paths, **self._load_json() }
     
-    pathspec = { **config }
+    def __dict__(self) -> dict:
+        return self._pathspec
+
+    def __getitem__(self, path):
+        return self._pathspec[path]
+
+    def __setitem__(self, path, data):
+        raise AcessViolationError(
+            """
+            Pathspec is read-only.
+            User defined paths must be specified under the `config/config.json` file.
+            """
+        )
+
+    def __missing__(self):
+        raise ValueError(
+            """
+            Path not found.
+            Check the file `config/config.json` for typos.
+            """
+        )
 
 
-# Functions
-def get_path( alias: str ):
-    path = pathspec.get( alias )
-    if (path is not None):
-        return path
-
-    raise HTTPException(
-        status_code=500,
-        detail="Invalid path"
-    )
+    def _load_json(self) -> dict:
+        config_file = Path( self._config_path, 'config.json' )
+        if ( config_file.is_file() ):
+            config = json.load( open(config_file) )['paths']
+            return config
+        
+        return {}
