ea786306146a1e8e403d86cc5b812d2ebe2db122
bryan-souza <bryans.souza@gmail.com>
2022-03-29 13:19:34 -0300

    Updated PathRouter class

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,24 +1,15 @@
 # Built-ins
+import os
 import json
 from pathlib import Path
-from typing import Any
 
 # Internals
 from app.exceptions import AcessViolationError
-from app.cerebrum import Identifier
 
-
-class PathRouterMeta(type):
+class PathRouter(object):
     _instances = {}
 
-    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
-
-class PathRouter(object, metaclass=PathRouterMeta):
-    _app_root_path = Path(__file__).resolve()
+    _app_root_path = Path(__file__).parent.resolve()
     _config_path   = Path(_app_root_path, 'config')
     _default_cache_path = Path(_app_root_path, 'cache')
 
@@ -28,9 +19,15 @@ class PathRouter(object, metaclass=PathRouterMeta):
         "CACHE_PATH"   : _default_cache_path
     } 
 
+    def __new__(cls):
+        if cls not in cls._instances:
+            instance = super().__new__(cls)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
 
     def __init__(self) -> None:
         self._pathspec = { **self._default_paths, **self._load_json() }
+        self._validate()
     
     def __dict__(self) -> dict:
         return self._pathspec
@@ -59,11 +56,13 @@ class PathRouter(object, metaclass=PathRouterMeta):
         config_file = Path( self._config_path, 'config.json' )
         if ( config_file.is_file() ):
             config = json.load( open(config_file) )['paths']
-            return config
+            for alias, path in config.items():
+                config.update({ alias: Path(path).resolve() })
         
         return {}
 
-
-def get_identifier():
-    instance = Identifier()
-    return instance
+    def _validate(self) -> None:
+        for path in self._pathspec.values():
+            if not ( path.exists() ):
+                absolute_path = path.resolve()
+                os.mkdir(absolute_path)
